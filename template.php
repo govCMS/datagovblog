a4ffe9798895db33769521574df611adb2eed17a
Jason Guo <jason.guo@xing.net.au>
2015-11-12 15:49:22 +1100

    update jQuery to 1.9.1

--- template.php
+++ template.php
@@ -306,4 +306,33 @@ function datagovau_bootstrap_facet_items_alter(&$build, &$settings) {
   }
 }
 
+function datagovau_bootstrap_js_alter(&$javascript) {
+  $node_admin_paths = array(
+    'node/*/edit',
+    'node/add',
+    'node/add/*',
+  );
+  $replace_jquery = TRUE;
+  if (path_is_admin(current_path())) {
+    $replace_jquery = FALSE;
+  }
+  else {
+    foreach ($node_admin_paths as $node_admin_path) {
+      if (drupal_match_path(current_path(), $node_admin_path)) {
+        $replace_jquery = FALSE;
+      }
+    }
+  }
+  // Swap out jQuery to use an updated version of the library.
+  if ($replace_jquery) {
+    $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'datagovau_bootstrap') . '/js/jquery.min.js';
+  }
+}
 
+function datagovau_bootstrap_html_tag($vars) {
+  if ($vars['element']['#tag'] == 'script') {
+    unset($vars['element']['#value_prefix'];
+    unset($vars['element']['#value_suffix'];
+  }
+  return theme_html_tag($vars);
+}
